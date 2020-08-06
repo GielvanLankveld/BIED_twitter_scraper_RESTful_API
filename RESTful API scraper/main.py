1b53976ec0f7088e2a87867b1c6c64c4b844fc05
MrGiel <gielvanlankveld@gmail.com>
2020-08-06 20:44:59 +0200

    Added all routes for the scraper's Flask API

--- RESTful API scraper/main.py
+++ RESTful API scraper/main.py
@@ -1,2 +1,33 @@
-from flask import Flask, request
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Base route'
+
+@app.route('/getallprojects/')
+def hello_world():
+    return 'Base route'
+
+@app.route('/getdatacollectionstatus/')
+def hello_world():
+    return 'Base route'
+    
+@app.route('/addtwitterhandle/')
+def hello_world():
+    return 'Base route'
+
+@app.route('/getresults/')
+def hello_world():
+    return 'Base route'
+
+@app.route('/startproject/')
+def hello_world():
+    return 'Base route'
+
+@app.route('/deleteproject/')
+def hello_world():
+    return 'Base route'
+
+if __name__ == '__main__':
+    app.run()
