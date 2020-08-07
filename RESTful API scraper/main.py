604bfe4ae3ce461b912793615528c1c91b1b38ad
MrGiel <gielvanlankveld@gmail.com>
2020-08-07 20:58:07 +0200

    Added route to poll scraping project status

--- RESTful API scraper/main.py
+++ RESTful API scraper/main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,27 +8,36 @@ def hello_world():
     return 'Base route'
 
 @app.route('/getallprojects/')
-def hello_world():
-    return 'Base route'
+def get_all_projects():
+    projects = ['BIED onderzoek September','Test onderzoek']
+    message = {
+        'status': 200,
+        'message': 'OK',
+        'content': projects
+    }
+    response = jsonify(projects)
+    response.status_code = 200
+    print(response)
+    return response
 
 @app.route('/getdatacollectionstatus/')
-def hello_world():
+def get_data_collection_status():
     return 'Base route'
     
 @app.route('/addtwitterhandle/')
-def hello_world():
+def add_twitter_handle():
     return 'Base route'
 
 @app.route('/getresults/')
-def hello_world():
+def get_results():
     return 'Base route'
 
 @app.route('/startproject/')
-def hello_world():
+def start_project():
     return 'Base route'
 
 @app.route('/deleteproject/')
-def hello_world():
+def delete_project():
     return 'Base route'
 
 if __name__ == '__main__':
